345e19b3f05e04aaf2f39778d5158ba5e81beea2
tobias svensson <tobias_e.svensson@hotmail.com>
2025-07-10 23:31:25 +0200

    Add project files.

---
+++ Sareq.API/Models/Note.cs
@@ -0,0 +1,11 @@
+﻿namespace Sareq.API.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public DateTime DateMade { get; set; } = DateTime.UtcNow;
+        public DateTime DateEdited { get; private set; } = DateTime.UtcNow;
+    }
+}
