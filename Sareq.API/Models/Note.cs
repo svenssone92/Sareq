ca6a4bb9b598d8f2298c2abc6808b1d6a0490826
tobias svensson <tobias_e.svensson@hotmail.com>
2025-07-11 00:19:26 +0200

    Updated Note Model

--- Sareq.API/Models/Note.cs
+++ Sareq.API/Models/Note.cs
@@ -5,7 +5,25 @@
         public int Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public string? Description { get; set; }
-        public DateTime DateMade { get; set; } = DateTime.UtcNow;
-        public DateTime DateEdited { get; private set; } = DateTime.UtcNow;
+
+        public DateTime DateMade { get; private set; } = DateTime.UtcNow;
+        public DateTime? DateEdited { get; set; }
+
+        public int ViewCount { get; private set; }
+        public DateTime? LastViewed { get; private set; }
+
+        public bool IsPinned { get; set; }
+
+        public void MarkEdited()
+        {
+            DateEdited = DateTime.UtcNow;
+        }
+
+        public void IncrementView()
+        {
+            ViewCount++;
+            LastViewed = DateTime.UtcNow;
+        }
+
     }
 }
